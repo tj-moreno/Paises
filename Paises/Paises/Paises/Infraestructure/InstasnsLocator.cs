e490a7f3f1b7dc659356d8ee0d01be7b4fce82d5
GRUPORIZEK\jose.moreno <jmoreno@rizekcacao.com>
2018-09-04 16:15:58 -0400

    Proyecto paises

---
+++ Paises/Paises/Paises/Infraestructure/InstasnsLocator.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Paises.Infraestructure
+{
+    using ViewModels;
+
+    class InstasnsLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructor        
+        public InstasnsLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
