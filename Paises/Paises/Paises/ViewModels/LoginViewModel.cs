60d1f28abeab562011b0d71d9b65ae84535f7304
GRUPORIZEK\jose.moreno <jmoreno@rizekcacao.com>
2018-09-04 18:17:25 -0400

    Creando las propiedades que se enlazaran a los controles

---
+++ Paises/Paises/Paises/ViewModels/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Paises.ViewModels
+{
+    using System.Windows.Input;
+    public class LoginViewModel
+    {
+        #region Propiedades
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsRememberd { get; set; }
+        #endregion
+
+        #region Commnads
+        public ICommand Entrat { get; set; }
+        public ICommand Register { get; set; }
+        #endregion
+
+        #region Constructor
+        public LoginViewModel()
+        {
+            this.IsRememberd = true;
+        }
+        #endregion
+    }
+}
--- Paises/Paises/Paises/ViewModels/MainViewModel.cs
+++ Paises/Paises/Paises/ViewModels/MainViewModel.cs
@@ -3,5 +3,15 @@ namespace Paises.ViewModels
 {
     public class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login { get; set; }
+        #endregion
+
+        #region constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
