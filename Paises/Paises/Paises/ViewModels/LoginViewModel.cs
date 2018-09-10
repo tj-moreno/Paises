72a9b2668efd9317aba53890ada6218d87256715
GRUPORIZEK\jose.moreno <jmoreno@rizekcacao.com>
2018-09-10 16:02:53 -0400

    Ajustes al Loging Clase  009

--- Paises/Paises/Paises/ViewModels/LoginViewModel.cs
+++ Paises/Paises/Paises/ViewModels/LoginViewModel.cs
@@ -1,25 +1,114 @@
 ﻿
 namespace Paises.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using System.ComponentModel;
     using System.Windows.Input;
-    public class LoginViewModel
+    using Xamarin.Forms;
+
+    public class LoginViewModel : BaseViewModel
     {
+        
+        #region Atributes
+        private string email;
+        private string password;
+        private bool isrunning;
+        private bool isrememberd;
+        private bool isenabled;
+        #endregion
+
         #region Propiedades
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRunning { get; set; }
+        public string Email {
+            get { return email; }
+            set { SetValue(ref this.email, value); }
+        }
+        public string Password {
+            get { return password; }
+            set { SetValue(ref this.password, value); }
+        }
+        public bool IsRunning {
+            get { return isrunning; }
+            set { SetValue(ref this.isrunning, value); }
+        }
         public bool IsRememberd { get; set; }
+        public bool IsEnabled {
+            get { return isenabled; }
+            set { SetValue(ref this.isenabled, value); }
+        }
         #endregion
 
         #region Commnads
-        public ICommand Entrat { get; set; }
-        public ICommand Register { get; set; }
+        public ICommand EntrarCommand
+        {
+            get
+            {
+                return new RelayCommand(Entrar);
+            }
+        }        
+
+        private async void Entrar()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "No se digito ningun E-Mail",
+                    "Aceptar"
+                    );
+
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "No se digito ninguna Contraseña",
+                    "Aceptar"
+                    );
+
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email!="alfonso125@gmail.com" || this.Password!="1234")
+            {
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Login Error",
+                    "Correo o contraseña invalida..",
+                    "Ok"
+                    );
+
+                this.IsRunning = false;
+                this.IsEnabled = true;
+
+                this.Password = string.Empty;
+
+                return;
+            }
+
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "Login",
+                    "Listo.....",
+                    "Ok"
+                    );
+        }
+
+        public ICommand RegisterCommand { get; set; }
         #endregion
 
         #region Constructor
         public LoginViewModel()
         {
             this.IsRememberd = true;
+            this.Email = "alfonso125@gmail.com";
         }
         #endregion
     }
